f2f59cd0e7d08ff84e16bb0c675086828ec89cbd
Kamilprogrammer <kg0390217@gmail.com>
2022-10-07 13:29:46 +0300

    kamil

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp3());
 }
 
 class MyApp extends StatelessWidget {
@@ -75,6 +75,152 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello world',
+      home: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Text(
+                  'Con 1',
+                  style: TextStyle(fontSize: 20),
+                ),
+                color: Colors.red,
+              ),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Text(
+                  'Con 2',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                color: Colors.blue,
+              ),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Text(
+                  'Con 3',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                color: Colors.greenAccent,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp3 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'hello world',
+      home: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(),
+        body: Column(
+          children: [
+            Divider(
+              color: Colors.white,
+              height: 200,
+            ),
+            FloatingActionButton.extended(
+              onPressed: () {},
+              label: Text('StrawberryTop 20 Most Popular'),
+            ),
+            Divider(
+              height: 20,
+              color: Colors.white,
+            ),
+            Container(
+              width: 250,
+              child: Text(
+                'Syrian cuisine is considered one of the most ancient and authentic kitchens in the Arab world. It is characterized by its diversity, due to the different civilizations that flourished in the region—the capital, Damascus, is the oldest continuously inhabited city in the world.',
+              ),
+            ),
+            Divider(
+              height: 50,
+              color: Colors.white,
+            ),
+            Container(
+              width: 500,
+              color: Colors.red,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '17 Reviews',
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [Icon(Icons.abc)],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
